9a73b33c9d85adff7bdacf20b6fe136f044d5f42
Ashok-Kumar <ap2714499@gmail.com>
2022-07-16 16:50:36 +0530

    Updates

---
+++ precache-manifest.7e5f6de28f2b6bb816acd293dca7d86d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4de90c86b41dbd33009d",
+    "url": "/text-utils/static/css/main.0543b940.chunk.css"
+  },
+  {
+    "revision": "4de90c86b41dbd33009d",
+    "url": "/text-utils/static/js/main.4de90c86.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/text-utils/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "e9f98c8c25c20724d10d",
+    "url": "/text-utils/static/js/2.e9f98c8c.chunk.js"
+  },
+  {
+    "revision": "7e5709d76ecc1923bb2a",
+    "url": "/text-utils/static/js/runtime~main.7e5709d7.js"
+  },
+  {
+    "revision": "cd300214e382aba5aa2271d73bb66bf6",
+    "url": "/text-utils/index.html"
+  }
+];
