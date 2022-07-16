64fbf10ac1f2ac0b4d783e9813e8dd90cc9fc043
Ashok-Kumar <ap2714499@gmail.com>
2022-07-16 14:51:55 +0530

    error solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import React from 'react'
 import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 function App() {
   const [mode, setMode] = useState("light");
-  const toggleMode = ()=>{
-    if(mode==="dark"){
+  const toggleMode = () => {
+    if (mode === "dark") {
       setMode("light");
       document.body.style.backgroundColor = "white";
-    }else{
+    } else {
       setMode("dark");
       document.body.style.backgroundColor = "#2a335a";
     }
   }
   return (
-   <>
-<Navbar title="TextUtils" about="About us" mode={mode} toggleMode={toggleMode}/>
-<div className="container my-3">
-<TextForm heading="Enter the text for analyze" mode={mode}/>
-</div>
-   </>
+    <>
+      <Navbar title="TextUtils" about="About us" mode={mode} toggleMode={toggleMode} />
+      <div className="container my-3">
+        <TextForm heading="Enter the text for analyze" mode={mode} />
+      </div>
+    </>
   );
 }
 
